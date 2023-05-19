c6dacfc99b80ef61ce4192cf964f4484b799d3ed
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-05-19 10:50:10 +0200

    Create basic layout, add reading and displaying files and folders

--- routes/main.ts
+++ routes/main.ts
@@ -1,8 +1,34 @@
 import express, {Request, Response, Router} from 'express';
+import * as filesystem from "fs";
+import path from 'path';
 
 const router: Router = express.Router();
 
-router.get('/', (req: Request, res: Response) => {
-	  res.render('root.hbs');
+const UPLOAD_DIR: string = "upload";
+
+router.get('/', (req: Request, res: Response): void => {
+	let files: string[] = [];
+	let folders: string[] = [];
+
+	filesystem.readdir(path.join(__dirname, "../..", UPLOAD_DIR), (error, files_found: string[]) => {
+		if (error) {
+			console.log(error);
+			return;
+		}
+
+		// Check if file is a folder
+		files_found.forEach((el: string): void => {
+			filesystem.statSync(path.join(__dirname, "../..", UPLOAD_DIR, el))
+				.isDirectory() ? folders.push(el) : files.push(el);
+		});
+
+		console.log(files);
+		console.log(folders);
+
+		res.render('root.hbs', {
+			files: files,
+			folders: folders
+		});
+	});
 });
 export default router;
--- server.ts
+++ server.ts
@@ -20,6 +20,7 @@ app.engine('hbs', create({
 app.set('view engine', 'hbs');
 app.set('views', './views');
 app.use(express.static('public'));
+// app.use(express.urlencoded({extended: true}));
 // ----Routes----
 app.use('/', main);
 
