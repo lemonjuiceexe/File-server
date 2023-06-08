9b17fcd81d562d8c37347c2b445272bbfffc3c32
lemonjuiceexe <74901431+lemonjuiceexe@users.noreply.github.com>
2023-06-08 15:28:25 +0200

    Configure prettier

--- routes/create.ts
+++ routes/create.ts
@@ -1,23 +1,51 @@
-import express, {Request, Response, Router} from 'express';
+import express, { Request, Response, Router } from "express";
 import * as filesystem from "fs";
 import * as filesystemPromises from "fs/promises";
-import path from 'path';
+import path from "path";
 
-import { IFiles } from '../types';
+import { IFiles } from "../types";
 import { UPLOAD_DIR, currentPath, setCurrentPath } from "../server";
 
 const router: Router = express.Router();
+const RESPONSE_CODES = {
+	OK: 0,
+	ALREADY_EXISTS: 1,
+	ERROR: 2
+};
 
 // Folder path relative to the upload directory
-async function createFolder(folderPath: string){
+async function createFolder(folderPath: string) {
 	await filesystemPromises.mkdir(path.join(UPLOAD_DIR, folderPath));
-	console.log("🎈 Created folder: " + folderPath);
+	console.log("🔨 Created folder: " + folderPath);
 }
 
-router.post('/createFolder', async (req: Request, res: Response): Promise<void> => {
+// File path relative to the upload directory
+async function createFile(filePath: string): Promise<number> {
+	// Check if file already exists
+	const fileExists: boolean = await filesystemPromises
+		.access(path.join(UPLOAD_DIR, filePath))
+		.then(() => true)
+		.catch(() => false);
+
+	if (!fileExists) {
+		let errorOccured = false;
+		await filesystemPromises
+			.appendFile(path.join(UPLOAD_DIR, filePath), "")
+			.then(() => console.log("🔨 Created file: " + filePath))
+			.catch(() => (errorOccured = true));
+		if (errorOccured) return RESPONSE_CODES.ERROR;
+	}
+
+	return new Promise<number>(resolve => resolve(fileExists ? RESPONSE_CODES.ALREADY_EXISTS : RESPONSE_CODES.OK));
+}
+
+router.post("/createFolder", async (req: Request, res: Response): Promise<void> => {
 	const folderName: string = req.body.folderName;
 	const folderPath: string = path.join(currentPath, folderName);
 	await createFolder(folderPath);
 	res.redirect("/");
+});
+router.post("/createFile", async (req: Request, res: Response): Promise<void> => {
+	console.log("a");
 });
 export default router;
--- server.ts
+++ server.ts
@@ -1,38 +1,44 @@
 // ----Imports----
 // Libraries
-import express, { Express, Request, Response, Router} from 'express';
-import { create } from 'express-handlebars';
-import path from 'path';
+import express, { Express, Request, Response, Router } from "express";
+import { create } from "express-handlebars";
+import path from "path";
 // import bodyParser from 'body-parser';
 // Custom
-import mainRouter from './routes/main';
-import createRouter from './routes/create';
+import mainRouter from "./routes/main";
+import createRouter from "./routes/create";
 
 // ----Variables----
 const app: Express = express();
 const PORT: number = 3000;
 export const UPLOAD_DIR: string = path.join(__dirname, "../", "upload");
 export let currentPath: string = "/superfolder";
-export function setCurrentPath(value: string):void { currentPath = value; }
+
+export function setCurrentPath(value: string): void {
+	currentPath = value;
+}
 
 // ----Config----
-app.engine('hbs', create({
-	layoutsDir: 'views/layouts',
-	partialsDir: 'views/partials',
-	defaultLayout: 'main',
-	extname: '.hbs'
-}).engine);
-app.set('view engine', 'hbs');
-app.set('views', './views');
-app.use(express.static('public'));
+app.engine(
+	"hbs",
+	create({
+		layoutsDir: "views/layouts",
+		partialsDir: "views/partials",
+		defaultLayout: "main",
+		extname: ".hbs"
+	}).engine
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ----Routes----
-app.use('/', mainRouter);
-app.use('/', createRouter);
+app.use("/", mainRouter);
+app.use("/", createRouter);
 
 // ----Server----
 app.listen(PORT, () => {
-	  console.log(`🎈 Server is running on port ${PORT}`);
+	console.log(`🎈 Server is running on port ${PORT}`);
 });
